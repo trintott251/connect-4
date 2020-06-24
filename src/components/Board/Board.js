626a668b6f780eb169f1eddc05e18dcf2abd4ba7
DESKTOP-1QUBPCJ\Trint <trintott@gmail.com>
2020-06-24 18:13:23 +0200

    Added function to place check on right place

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -27,7 +27,7 @@ export default class Board extends Component {
 
         for(let i = 0; i < 7; i++){
             data.push(
-                <td column={i} key={i}>
+                <td column={i} row={row} key={i} checked={false}>
                     <div className={styles.circle} onClick={(e) => this.clickBox(e)}>{row} - {i}</div>
                 </td>
             )
@@ -37,15 +37,44 @@ export default class Board extends Component {
 
     clickBox = (e) => {
         let target = e.target;
-        console.log(target.style);
+        let parent = target.parentNode;
+        let column = parent.getAttribute("column");
+        // let row = parent.parentNode.getAttribute("row");
+
+        let boxToCheck = this.getBoxToCheck(column);
+        console.log("boxToCheck", boxToCheck);
 
         if (target.style.cssText) { 
             return;
         } else {
             let playerTurn = this.state.playerTurn;
-            playerTurn == 1 ? target.style.backgroundColor = "red" : target.style.backgroundColor = "black";
+            playerTurn == 1 ? boxToCheck.childNodes[0].style.backgroundColor = "red" : boxToCheck.childNodes[0].style.backgroundColor = "blue";
             playerTurn == 1 ? this.setState({playerTurn:2}) : this.setState({playerTurn:1});
+            boxToCheck.setAttribute('checked', 'true');
         }
+    }
+
+    getBoxToCheck = (column) => {
+        let allBoxesInColumn = [];
+        var allElements = document.getElementsByTagName('td');
+        for (var i = 0, n = allElements.length; i < n; i++)
+        {
+          if (allElements[i].getAttribute("column") == column)
+          {
+            allBoxesInColumn.push(allElements[i]);
+          }
+        }
+
+        let boxToCheck;
+        for(let a = allBoxesInColumn.length; a > 0; a--) {
+            let item = allBoxesInColumn[a - 1];
+
+            if(item.getAttribute("checked") == null) {
+                boxToCheck = item;
+                return boxToCheck;
+            }
+        }
+        return null;
     }
 
     render() {
