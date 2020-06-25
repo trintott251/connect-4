5f6da6b5cbd0be9f9c64ad70a519314c7b0369b5
DESKTOP-1QUBPCJ\Trint <trintott@gmail.com>
2020-06-25 21:58:01 +0200

    Added draw function

--- src/components/GameOverDialog/GameOverDialog.js
+++ src/components/GameOverDialog/GameOverDialog.js
@@ -3,15 +3,19 @@ import styles from "./GameOverDialog.module.scss";
 
 export default class GameOverDialog extends Component {
     render() {
-        let { playerWon } = this.props;
+        let { playerWon, title, isDraw } = this.props;
         let colorWon = playerWon == 1 ? "Red" : "Blue";
 
         return (
             <div className={styles.overlay}>
                 <div className={styles.dialog}>
                     <div className={styles.text}>
-                        <p className={styles["game-over"]}>Game over</p>
-                        <p className={styles["player-won"]}>Player <span className={colorWon == "Red" ? styles.red : styles.blue}>{colorWon}</span> Won</p>
+                        <p className={styles["game-over"]}>{title}</p>
+                        {
+                            isDraw
+                            ? <p className={styles["player-won"]}>It seems it's a draw!</p>
+                            : <p className={styles["player-won"]}>Player <span className={colorWon == "Red" ? styles.red : styles.blue}>{colorWon}</span> Won</p>
+                        }
                     </div>
                     <button className={styles["play-again"]} onClick={this.props.playAgain}>
                         Play again
