a2c25f0e0296fa9d1b49b4df71ff2d79b8285f58
DESKTOP-1QUBPCJ\Trint <trintott@gmail.com>
2020-06-25 11:45:14 +0200

    Created game over function

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -6,7 +6,8 @@ export default class Board extends Component {
         super(props);
         this.state = {
             playerTurn: 1,
-            gameOver: false
+            gameOver: false,
+            lastCheckedBox: null
         };
     }
 
@@ -40,25 +41,29 @@ export default class Board extends Component {
     }
 
     clickBox = (e) => {
+        if (this.state.gameOver) {
+            return;
+        }
+
         let target = e.target;
         let parent = target.parentNode;
         let column = parent.getAttribute("column");
-        // let row = parent.parentNode.getAttribute("row");
 
         let boxToCheck = this.getBoxToCheck(column);
-        // console.log("boxToCheck", boxToCheck);
+        
+        if (boxToCheck !== null) {
+            if (boxToCheck.getAttribute("checked") !== null) { 
+                return;
+            } else {
+                let playerTurn = this.state.playerTurn;
+                playerTurn == 1 ? boxToCheck.childNodes[0].style.backgroundColor = "red" : boxToCheck.childNodes[0].style.backgroundColor = "blue";
+                boxToCheck.setAttribute('checked', 'true');
+                boxToCheck.setAttribute('ownedby', this.state.playerTurn);
+                playerTurn == 1 ? this.setState({playerTurn:2}) : this.setState({playerTurn:1});
+            }
 
-        if (target.style.cssText) { 
-            return;
-        } else {
-            let playerTurn = this.state.playerTurn;
-            playerTurn === 1 ? boxToCheck.childNodes[0].style.backgroundColor = "red" : boxToCheck.childNodes[0].style.backgroundColor = "blue";
-            boxToCheck.setAttribute('checked', 'true');
-            boxToCheck.setAttribute('ownedby', this.state.playerTurn);
-            playerTurn === 1 ? this.setState({playerTurn:2}) : this.setState({playerTurn:1});
+            this.checkIfWin(boxToCheck);
         }
-
-        this.checkIfWin(parent);
     }
 
     // clickBox = (e) => {
@@ -94,7 +99,7 @@ export default class Board extends Component {
         var allElements = document.getElementsByTagName('td');
         for (var i = 0, n = allElements.length; i < n; i++)
         {
-          if (allElements[i].getAttribute("column") === column)
+          if (allElements[i].getAttribute("column") == column)
           {
             allBoxesInColumn.push(allElements[i]);
           }
@@ -106,6 +111,7 @@ export default class Board extends Component {
 
             if(item.getAttribute("checked") == null) {
                 boxToCheck = item;
+                this.setState({lastCheckedBox: item});
                 return boxToCheck;
             }
         }
@@ -113,16 +119,11 @@ export default class Board extends Component {
     }
 
     checkIfWin = (target) => {
-        // let vertical = this.checkVertical(target);
-        // let horizontal = this.checkHorizontal(target);
-        let diagonal = this.checkDiagonal(target);
-        console.log("horizontal check: ", diagonal)
-
-        // if (this.checkVertical() || this.checkHorizontal() || this.checkDiagonal()) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
+        if (this.checkVertical(target) || this.checkHorizontal(target) || this.checkDiagonal(target)) {
+            let player = this.state.playerTurn == 1 ? "Red" : "Blue";
+            this.setState({ gameOver: true });
+            console.log(player, " player wins the game!")
+        }
     }
 
     checkVertical = (target) => {
@@ -132,9 +133,9 @@ export default class Board extends Component {
         let allBoxesInColumn = [];
         var allElements = document.getElementsByTagName('td');
         for (var i = 0, n = allElements.length; i < n; i++) {
-            if (allElements[i].getAttribute("column") === column) {
+            if (allElements[i].getAttribute("column") == column) {
                 if(allElements[i].getAttribute("ownedby") !== null) {
-                    if(allElements[i].getAttribute("ownedby") === turn) {
+                    if(allElements[i].getAttribute("ownedby") == turn) {
                         allBoxesInColumn.push(parseInt(allElements[i].getAttribute("row")));
                     }
                 }
@@ -155,9 +156,9 @@ export default class Board extends Component {
         let allBoxesInRow = [];
         var allElements = document.getElementsByTagName('td');
         for (var i = 0, n = allElements.length; i < n; i++) {
-            if (allElements[i].getAttribute("row") === row) {
+            if (allElements[i].getAttribute("row") == row) {
                 if(allElements[i].getAttribute("ownedby") !== null) {
-                    if(allElements[i].getAttribute("ownedby") === turn) {
+                    if(allElements[i].getAttribute("ownedby") == turn) {
                         allBoxesInRow.push(parseInt(allElements[i].getAttribute("column")));
                     }
                 }
@@ -172,8 +173,7 @@ export default class Board extends Component {
     }
 
     checkDiagonal = (target) => {
-        let diagonalRightTop = this.diagonalRightTop(target);
-        console.log("diagonalRightTop check: ", diagonalRightTop)
+        return this.diagonalRightTop(target) || this.diagonalLeftTop(target);
     }
 
     diagonalRightTop = (target) => {
@@ -181,56 +181,91 @@ export default class Board extends Component {
         let row = target.getAttribute("row");
         let column = target.getAttribute("column");
 
-        let allBoxesDiagonalRightTop = [];
+        let allBoxesDiagonal = [];
         var allElements = document.getElementsByTagName('td');
 
         for (var i = 0, n = allElements.length; i < n; i++) {
-            if ((allElements[i].getAttribute("row") === row) && (allElements[i].getAttribute("column") === column)) {
-                allBoxesDiagonalRightTop.push(parseInt(allElements[i].getAttribute("column")));
+            if ((allElements[i].getAttribute("row") == row) && (allElements[i].getAttribute("column") == column)) {
+                allBoxesDiagonal.push(parseInt(allElements[i].getAttribute("column")));
             }
 
             for (var a = 1; a < 4; a++) {
-                if ((allElements[i].getAttribute("row") === (parseInt(row) + a)) && (allElements[i].getAttribute("column") === (parseInt(column) - a))) {
+                if ((allElements[i].getAttribute("row") == (parseInt(row) + a)) && (allElements[i].getAttribute("column") == (parseInt(column) - a))) {
                     if(allElements[i].getAttribute("ownedby") !== null) {
-                        if(allElements[i].getAttribute("ownedby") === turn) {
-                            allBoxesDiagonalRightTop.push(parseInt(allElements[i].getAttribute("column")));
+                        if(allElements[i].getAttribute("ownedby") == turn) {
+                            allBoxesDiagonal.push(parseInt(allElements[i].getAttribute("column")));
                         }
                     }
                 }
             }
             for (var a = 1; a < 4; a++) {
-                if ((allElements[i].getAttribute("row") === (parseInt(row) - a)) && (allElements[i].getAttribute("column") === (parseInt(column) + a))) {
+                if ((allElements[i].getAttribute("row") == (parseInt(row) - a)) && (allElements[i].getAttribute("column") == (parseInt(column) + a))) {
                     if(allElements[i].getAttribute("ownedby") !== null) {
-                        if(allElements[i].getAttribute("ownedby") === turn) {
-                            allBoxesDiagonalRightTop.push(parseInt(allElements[i].getAttribute("column")));
+                        if(allElements[i].getAttribute("ownedby") == turn) {
+                            allBoxesDiagonal.push(parseInt(allElements[i].getAttribute("column")));
                         }
                     }
                 }
             }
         }
 
-        allBoxesDiagonalRightTop.sort(function(a, b) {
-            return a - b;
-        });
-
-        if (this.checkConsec(allBoxesDiagonalRightTop)) {
+        if (this.checkConsec(allBoxesDiagonal)) {
             return true;
         } else {
             return false;
         }
     }
 
-    diagonalLeftTop = () => {
-        
+    diagonalLeftTop = (target) => {
+        let turn = this.state.playerTurn;
+        let row = target.getAttribute("row");
+        let column = target.getAttribute("column");
+
+        let allBoxesDiagonal = [];
+        var allElements = document.getElementsByTagName('td');
+
+        for (var i = 0, n = allElements.length; i < n; i++) {
+            if ((allElements[i].getAttribute("row") == row) && (allElements[i].getAttribute("column") == column)) {
+                allBoxesDiagonal.push(parseInt(allElements[i].getAttribute("column")));
+            }
+
+            for (var a = 1; a < 4; a++) {
+                if ((allElements[i].getAttribute("row") == (parseInt(row) + a)) && (allElements[i].getAttribute("column") == (parseInt(column) + a))) {
+                    if(allElements[i].getAttribute("ownedby") !== null) {
+                        if(allElements[i].getAttribute("ownedby") == turn) {
+                            allBoxesDiagonal.push(parseInt(allElements[i].getAttribute("column")));
+                        }
+                    }
+                }
+            }
+            for (var a = 1; a < 4; a++) {
+                if ((allElements[i].getAttribute("row") == (parseInt(row) - a)) && (allElements[i].getAttribute("column") == (parseInt(column) - a))) {
+                    if(allElements[i].getAttribute("ownedby") !== null) {
+                        if(allElements[i].getAttribute("ownedby") == turn) {
+                            allBoxesDiagonal.push(parseInt(allElements[i].getAttribute("column")));
+                        }
+                    }
+                }
+            }
+        }
+
+        if (this.checkConsec(allBoxesDiagonal)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     checkConsec = (array) => {
         var current = null;
         var cnt = 0;
 
+        array.sort(function(a, b) {
+            return a - b;
+        });
+
         for (var i = 0; i < array.length; i++) {
-            // Also need to make sure the next array item is a consecutive increase.
-            if (array[i] !== current && array[i] === array[i-1] + 1) {
+            if (array[i] != current && array[i] === array[i-1] + 1) {
                 if (cnt > 3) {
                     return true;
                 }
